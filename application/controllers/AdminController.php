6c1403991c3f0435c0654abd51b5e1d3c49495a6
droopram <droopram@gmail.com>
2014-06-04 13:31:50 +0200

    Uh can't remember
    Just syncing to get your updates really.

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -1,6 +1,14 @@
 <?php
 class AdminController extends Zend_Controller_Action{
 	public function init(){
+// 		if(!$this->getViewer()){
+		if(1){
+	    $action = $this->getRequest()->getActionName();
+			if($action!='login'){
+				$this->redirect('admin/login');
+			}
+		}
+	        
 		$this->_helper->layout()->setLayout('admin-layout');
 	}
 	
@@ -52,6 +60,46 @@ class AdminController extends Zend_Controller_Action{
 		
 	}
 	
+	protected function _process($values)
+	{
+	    // Get our authentication adapter and check credentials
+	    $adapter = $this->_getAuthAdapter();
+	    $adapter->setIdentity($values['username']);
+	    $adapter->setCredential($values['password']);
+	
+	    $auth = Zend_Auth::getInstance();
+	    $result = $auth->authenticate($adapter);
+	    if ($result->isValid()) {
+	        $user = $adapter->getResultRowObject();
+	        $auth->getStorage()->write($user);
+	        return true;
+	    }
+	    return false;
+	}
+	
+	protected function _getAuthAdapter() {
+	    $table = new Application_Model_DbTable_User();
+	    $dbAdapter = $table->getAdapter();
+	    $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
+	    	
+	    $authAdapter
+	    ->setTableName($table->info('name'))
+	    ->setIdentityColumn('username')
+	    ->setCredentialColumn('password')
+	    ->setCredentialTreatment('(sha1(concat(?, salt)))');
+	    return $authAdapter;
+	}
+	
+	protected function getViewer(){
+	    $auth = Zend_Auth::getInstance();
+	    if ($auth->hasIdentity()) {
+	        $table = new Application_Model_DbTable_User();
+	        $user = $table->find($auth->getIdentity()->user_id)->current();
+	        if($user) return $user;
+	        else return false;
+	    } else
+	        return false;
+	}
 	
 	
 	
